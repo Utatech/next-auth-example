1a1a531bb1b17f23c10bf0547526baabeda19746
Utatech <prawiraputra64@gmail.com>
2022-05-16 13:29:37 +0700

    add: add project

--- pages/admin/_middleware.js
+++ pages/admin/_middleware.js
@@ -3,6 +3,9 @@ import { withAuth } from "next-auth/middleware"
 // More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
 export default withAuth({
   callbacks: {
-    authorized: ({ token }) => token?.userRole === "admin",
+    authorized: ({ token }) => {
+      console.log(token?.userRole);
+      token?.userRole === "admin"
+    },
   },
 })
